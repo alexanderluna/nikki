8af9bb6f7bb45965866f1f63fb92930cc1dabaab
Mycroft1891 <mycroft1891@gmail.com>
2019-02-27 21:37:18 +0100

    Delete: googlesheet api and API config file

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  clientId: '596269888326-dnlcr5htulojststiu9vuqn7qi97772s.apps.googleusercontent.com',
-  apiKey: 'AIzaSyDccQUzQ1B1TjwCLDCRJXU32s3OWGqFntI',
-  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  spreadsheetId: '1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo',
-  scopes: 'https://www.googleapis.com/auth/spreadsheets.readonly'
-};
