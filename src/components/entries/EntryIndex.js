a91227c4a18879d927070f04d6da76c8856674a1
Mycroft1891 <mycroft1891@gmail.com>
2019-03-05 21:45:13 +0100

    Update: render all entries

--- src/components/entries/EntryIndex.js
+++ src/components/entries/EntryIndex.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import Entry from './partials/Entry';
 
-class EntryIndex extends Component {
-  render() {
-    return (
-      <div>
-        Show all Entries
-      </div>
-    )
-  }
-}
+const EntryIndex = ({ entries }) => (
+  <div>
+    <ul>
+      {entries.map(({ id, content }) => (
+        <Entry key={id} id={id} content={content} />
+      ))}
+    </ul>
+  </div>
+);
 
-export default EntryIndex
+const mapStateToProps = state => ({
+  entries: Object.values(state.entries),
+});
+
+export default connect(mapStateToProps)(EntryIndex);
