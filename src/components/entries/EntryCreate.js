eab1a46093cce05fa0ad75269142d19b43403ba7
Mycroft1891 <mycroft1891@gmail.com>
2019-03-09 21:49:04 +0100

    Update: create page to display redux-form component

--- src/components/entries/EntryCreate.js
+++ src/components/entries/EntryCreate.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createEntry } from '../../actions';
+import EntryForm from './partials/EntryForm';
 
 class EntryCreate extends Component {
+  onSubmit = (formValues) => {
+    this.props.createEntry(formValues);
+  };
+
   render() {
     return (
       <div>
-        <h1>Create Entry</h1>
+        <h3>Create Entry</h3>
+        <EntryForm onSubmit={this.onSubmit} />
       </div>
-    )
+    );
   }
 }
 
-export default EntryCreate
+export default connect(null, { createEntry })(EntryCreate);
