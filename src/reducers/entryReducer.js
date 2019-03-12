49a1d7ef46e29ecd9d95ca0735c40a3c9e4d040f
Mycroft1891 <mycroft1891@gmail.com>
2019-03-12 14:05:57 +0100

    Update: set initial state and implement edit & delete entry reducers

--- src/reducers/entryReducer.js
+++ src/reducers/entryReducer.js
@@ -3,26 +3,34 @@ import {
   IMPORT_DIARY,
   CREATE_ENTRY,
   EDIT_ENTRY,
-  FETCH_ENTRY,
-  FETCH_ENTRIES,
   DELETE_ENTRY,
 } from '../actions/types';
 
-export default (state = {}, action) => {
+const initialState = {
+  _oj5szld68: {
+    createdAt: 'Thu Aug 08 2019 00:49:32 GMT+0200 (CEST)',
+    updatedAt: 'Thu Aug 08 2019 00:55:00 GMT+0200 (CEST)',
+    content: '<b>Some bold text</b> and some <i>italic</i> text',
+    id: '_oj5szld68',
+  },
+  _oj5szl468: {
+    createdAt: 'Thu Aug 08 2019 00:49:32 GMT+0200 (CEST)',
+    updatedAt: 'Thu Aug 08 2019 00:55:00 GMT+0200 (CEST)',
+    content: '<b>Some bold text</b> and some <i>italic</i> text',
+    id: '_oj5szl468',
+  },
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case IMPORT_DIARY:
       return { ...state, ..._.mapKeys(action.payload, 'id') };
     case CREATE_ENTRY:
-      console.log(action.payload);
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_ENTRY:
-      return state;
-    case FETCH_ENTRY:
-      return state;
-    case FETCH_ENTRIES:
-      return state;
+      return { ...state, [action.payload.id]: action.payload };
     case DELETE_ENTRY:
-      return state;
+      return _.omit(state, action.payload);
     default:
       return state;
   }
