99bb302aa72007871aa92e17f6faf85f0155d6cb
Mycroft1891 <mycroft1891@gmail.com>
2019-03-12 14:05:08 +0100

    Update: remove unnecessary action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,4 @@
 export const CREATE_ENTRY = 'CREATE_ENTRY';
 export const EDIT_ENTRY = 'EDIT_ENTRY';
-export const FETCH_ENTRY = 'FETCH_ENTRY';
-export const FETCH_ENTRIES = 'FETCH_ENTRIES';
 export const IMPORT_DIARY = 'IMPORT_DIARY';
 export const DELETE_ENTRY = 'DELETE_ENTRY';
