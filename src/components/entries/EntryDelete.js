5ba1b7d57813b25bb4f87e7e8159f15468ae7472
Mycroft1891 <mycroft1891@gmail.com>
2019-03-14 14:07:52 +0100

    Update: redux implementation for delete action

--- src/components/entries/EntryDelete.js
+++ src/components/entries/EntryDelete.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteEntry } from '../../actions';
 
 class EntryDelete extends Component {
+  componentDidMount() {
+    const { match: { params } } = this.props;
+    this.props.deleteEntry(params.id);
+  }
+
   render() {
     return (
       <div>
         <h1>Delete Entry</h1>
       </div>
-    )
+    );
   }
 }
 
-export default EntryDelete
+export default connect(null, { deleteEntry })(EntryDelete);
