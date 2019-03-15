e8f045300e4e9a70eb7dcfdc489442a8ace898bc
Mycroft1891 <mycroft1891@gmail.com>
2019-03-15 14:09:15 +0100

    Fix: formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div>
-      <Link to="/">Home</Link> |
-      <Link to="/create">Create</Link> |
-      <Link to="/index">All Entries</Link> |
+      <Link to="/">Home</Link>
+      <Link to="/create">Create</Link>
+      <Link to="/index">All Entries</Link>
     </div>
   );
 }
