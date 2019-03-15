a1768312ba83deaae6680a86be5b4cefe5188f05
Mycroft1891 <mycroft1891@gmail.com>
2019-03-15 14:08:46 +0100

    Update: setup edit component with redux action and form control

--- src/components/entries/EntryEdit.js
+++ src/components/entries/EntryEdit.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { editEntry } from '../../actions';
+import EntryForm from './partials/EntryForm';
 
 class EntryEdit extends Component {
+  onSubmit = (formValues) => {
+    const { match: { params } } = this.props;
+    this.props.editEntry(params.id, formValues);
+  }
+
   render() {
+    const { entry } = this.props;
     return (
       <div>
-        <h1>Edit Entry</h1>
+        <h3>Edit Entry</h3>
+        <EntryForm
+          initialValues={_.pick(entry, ['content', 'createdAt'])}
+          onSubmit={this.onSubmit}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default EntryEdit
+const mapStateToProps = (state, ownProps) => ({
+  entry: state.entries[ownProps.match.params.id],
+});
+
+export default connect(mapStateToProps, { editEntry })(EntryEdit);
