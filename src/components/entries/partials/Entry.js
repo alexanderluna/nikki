1157a961ac4174d316f7ef0482d3486d9e10b901
Mycroft1891 <mycroft1891@gmail.com>
2019-03-16 14:09:45 +0100

    Update: add links for edit and delete actions

--- src/components/entries/partials/Entry.js
+++ src/components/entries/partials/Entry.js
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom';
 
 export default function Entry({ id, content }) {
   return (
-    <Link to={`/show/${id}`}>
-      <p dangerouslySetInnerHTML={{ __html: content }} />
-    </Link>
+    <div className="">
+      <Link to={`/show/${id}`}>
+        <p dangerouslySetInnerHTML={{ __html: content }} />
+      </Link>
+      <Link to={`/edit/${id}`}>
+        Edit
+      </Link>
+      <Link to={`/delete/${id}`}>
+        Delete
+      </Link>
+    </div>
   );
 }
