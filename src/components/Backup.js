797b3d3c595494e10fc2228c39960333ac5dae3a
Mycroft1891 <mycroft1891@gmail.com>
2019-03-24 12:56:08 +0100

    Update: styling and isolate component

--- src/components/Backup.js
+++ src/components/Backup.js
@@ -1,31 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { makeStyles } from '@material-ui/core/styles/';
+import Fab from '@material-ui/core/Fab';
+import CloudDownload from '@material-ui/icons/CloudDownloadOutlined';
 
-class Backup extends Component {
-  get downloadString() {
-    const { entries } = this.props;
-    const encodedJson = encodeURIComponent(JSON.stringify(entries));
+const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+    position: 'fixed',
+    bottom: '0px',
+    right: '0px',
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  link: {
+    color: '#FFF',
+    textDecoration: 'none',
+  },
+}));
+
+const Backup = ({ diary }) => {
+  const classes = useStyles();
+
+  const downloadLink = () => {
+    const encodedJson = encodeURIComponent(JSON.stringify(diary));
     return `data:text/json;charset=utf-8,${encodedJson}`;
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <a
-          href={this.downloadString}
-          download="nikki.json"
-          style={{ position: 'fixed', bottom: '0', right: '0' }}
+  const hasEntries = () => (Object.keys(diary.entries).length >= 1);
+
+  return (
+    <div>
+      {hasEntries() && (
+        <Fab
+          color="primary"
+          variant="extended"
+          className={classes.fab}
+          aria-label="download"
         >
-          Download
-        </a>
-      </div>
-    );
-  }
-}
+          <CloudDownload className={classes.extendedIcon} />
+          <a
+            className={classes.link}
+            href={downloadLink()}
+            download="nikki.json"
+          >
+            Download
+          </a>
+        </Fab>
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
-  entries: _.omit(state, 'form'),
+  diary: _.omit(state, 'form'),
 });
 
 export default connect(mapStateToProps)(Backup);
