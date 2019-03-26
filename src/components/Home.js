9b82ad62361864046106d5a2d746afbdb820c3e1
Mycroft1891 <mycroft1891@gmail.com>
2019-03-26 12:58:57 +0100

    Update: switch external styling to internal, remove redux and download implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,54 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { importDiary } from '../actions';
-import './Home.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import { Container, Button } from '@material-ui/core';
+import { NoteAddOutlined } from '@material-ui/icons';
+import UploadField from './UploadField';
 
-class Home extends Component {
-  importDiary = (data) => {
-    const diary = JSON.parse(data.target.result);
-    this.props.importDiary(diary);
-  }
+const styles = theme => ({
+  header: {
+    height: '100vh',
+    backgroundImage: 'url(/nikki2.jpg)',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+  subHeader: {
+    paddingTop: '25vh',
+    textAlign: 'center',
+    color: '#d5d5d5',
+    fontFamily: 'pt mono, monospace, serif',
+  },
+  actionButton: {
+    margin: theme.spacing(1),
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+});
 
-  readBackup = ({ target: { files } }) => {
-    if (!files[0]) return;
-
-    const reader = new FileReader();
-    reader.onload = this.importDiary;
-    reader.readAsText(files[0]);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <label
-          className="upload"
-          htmlFor="upload"
-          onChange={this.readBackup}
+const Home = ({ classes }) => (
+  <div>
+    <Container className={classes.header}>
+      <Container className={classes.subHeader} maxWidth="sm">
+        <h2>
+          Create your private diary without having
+          to connect to a server
+        </h2>
+        <UploadField />
+        <Button
+          to="/index"
+          component={Link}
+          variant="contained"
+          color="secondary"
+          className={classes.actionButton}
         >
-          <span aria-hidden="true" />
-          <input
-            className="upload"
-            type="file"
-            accept="application/json"
-          />
-        </label>
-      </div>
-    );
-  }
-}
+          Create Diary
+          <NoteAddOutlined className={classes.rightIcon} />
+        </Button>
+      </Container>
+    </Container>
+  </div>
+);
 
-export default connect(null, { importDiary })(Home);
+export default withStyles(styles)(Home);
