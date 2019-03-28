25dae50ec6ae37dc60c8cc0aa20cdd5f43985c7c
Mycroft1891 <mycroft1891@gmail.com>
2019-03-28 13:01:10 +0100

    Update: display modal for confirmation and redesign redux action call

--- src/components/entries/EntryDelete.js
+++ src/components/entries/EntryDelete.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { deleteEntry } from '../../actions';
+import Modal from './partials/Modal';
+import history from '../../history';
 
 class EntryDelete extends Component {
-  componentDidMount() {
+  handleClose = () => {
+    history.push('/index');
+  }
+
+  handleDelete = () => {
     const { match: { params } } = this.props;
     this.props.deleteEntry(params.id);
   }
@@ -11,7 +17,13 @@ class EntryDelete extends Component {
   render() {
     return (
       <div>
-        <h1>Delete Entry</h1>
+        <Modal
+          title="Delete Entry"
+          handleClose={this.handleClose}
+          handleDelete={this.handleDelete}
+        >
+          Are you sure want to delete this entry ?
+        </Modal>
       </div>
     );
   }
